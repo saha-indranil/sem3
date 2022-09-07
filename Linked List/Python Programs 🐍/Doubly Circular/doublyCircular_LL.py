4e4e8985dae4646bb422ec4913e381d35ea361ad
Indranil Saha <91204567+saha-indranil@users.noreply.github.com>
2022-09-07 16:24:58 +0530

    Update doublyCircular_LL.py

--- Linked List/Python Programs 🐍/Doubly Circular/doublyCircular_LL.py
+++ Linked List/Python Programs 🐍/Doubly Circular/doublyCircular_LL.py
@@ -1,14 +1,14 @@
 # Write a python program to create a Double Circular
 # linked list along with the following functions:
-# a) to display the elements of a Double Linked List.
-# b) to add an element at beginning of a Double linked list.
-# c) to add an element at the end of a Double linked list.
-# d) to add an element at any user given position of a Double linked list.
-# e) to search a user given element in a Double linked list.
-# f) to count the number of nodes in a Double linked list.
-# g) to delete a node at beginning of a Double linked list.
-# h) to delete a node at end of a Double linked list.
-# i) to delete a node from any position of a Double linked list.
+# a) to display the elements of a Double Circular Linked List.
+# b) to add an element at beginning of a Double Circular linked list.
+# c) to add an element at the end of a Double Circular linked list.
+# d) to add an element at any user given position of a Double Circular linked list.
+# e) to search a user given element in a Double Circular linked list.
+# f) to count the number of nodes in a Double Circular linked list.
+# g) to delete a node from beginning of a Double Circular linked list.
+# h) to delete a node from end of a Double Circular linked list.
+# i) to delete a node from any position of a Double Circular linked list.
 # (Create a menu bar for accessing the functions)
 
 class Node:
