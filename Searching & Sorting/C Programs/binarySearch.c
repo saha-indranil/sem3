02a1e65553b47529c7e3151f8a9cedc656283c3c
Indranil Saha <sahaindranil2004@gmail.com>
2022-09-18 03:16:24 +0530

    added

--- Searching & Sorting/C Programs/binarySearch.c
+++ Searching & Sorting/C Programs/binarySearch.c
@@ -23,19 +23,13 @@ int binarySearch(int arr[], int n, int key)
     int low = 0, mid, high = n - 1;
     while (low <= high)
     {
-        mid = (low + high) / 2;
+        mid = low + (high - low) / 2;
         if (arr[mid] == key)
-        {
             return mid;
-        }
         else if (arr[mid] < key)
-        {
             low = mid + 1;
-        }
         else if (arr[mid] > key)
-        {
             high = mid - 1;
-        }
     }
     return -1;
 }
